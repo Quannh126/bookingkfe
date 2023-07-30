5129cd98cae4e944da571b684b8e164c55f5b7ef
Quan <quan.nh148983@sis.hust.edu.vn>
2023-07-30 23:11:03 +0700

    Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//     enabled: process.env.ANALYZE==='true',
+// });
+// module.exports = (phase, nextConfig) => {
+//     nextConfig = {
+//         reactStrictMode: true,
+//         swcMinify: true,
+//         env: {
+//             API_URL: process.env.API_URL,
+//         },
+//         compiler: {
+//             styledComponents: true,
+//         },
+//     };
+//     return withBundleAnalyzer(nextConfig);
+// };
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
@@ -9,5 +26,4 @@ const nextConfig = {
         styledComponents: true,
     },
 };
-
 module.exports = nextConfig;
